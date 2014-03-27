4cc540bb78467c22a32f0822ec4cef5ec45dcf63
vikytech <famous.vikhyath@gmail.com>
2014-03-27 15:44:49 +0530

    Moving back third party libraries to assets and adding pre compilation for production

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module TrackerServer
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += %w(application.js,application.css)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
